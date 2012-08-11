6935de156f21e3b75f4e710b7fceec42392cc90c
Carter Thaxton <carter.thaxton@gmail.com>
2012-08-10 20:44:54 -0700

    Refactor player choices out to separate module

---
+++ lib/choices.rb
@@ -0,0 +1,155 @@
+module Dominion
+	module Choices
+		attr_reader :choice_in_progress
+
+    def ask(message, options = {})
+      options[:message] = message
+      options[:type] = :bool
+      options[:multiple] = false
+      choose(options)
+    end
+    
+    def choose_card(message, options = {})
+      options[:message] = message
+      options[:type] = :card
+      options[:multiple] = false
+      choose(options)
+    end
+    
+    def choose_cards(message, options = {})
+      options[:message] = message
+      options[:type] = :card
+      options[:multiple] = true
+      choose(options)
+    end
+
+    def choose_one(messages, symbols, options = {})
+      if symbols.count > 1
+        options[:message] = 'Choose one: ' + messages.join(' or ')
+        options[:messages] = messages
+        options[:type] = :symbol
+        options[:multiple] = false
+        options[:restrict_to] = symbols
+        options[:required] = true
+        choose(options)
+      else
+        # Don't bother asking for zero or one choices
+        symbols.first
+      end
+    end
+
+    def choose_two(messages, symbols, options = {})
+      options[:message] = 'Choose two: ' + messages.join(' or ')
+      options[:messages] = messages
+      options[:type] = :symbol
+      options[:multiple] = true
+      options[:restrict_to] = symbols
+      options[:unique] = true
+      options[:count] = 2
+      choose(options)
+    end
+
+    def choose(options)
+      @choice_in_progress = options
+
+      # use choice if given directly in call to play
+      # otherwise defer to strategy if available
+      response = if @play_choice
+        handle_response(@play_choice)
+      elsif @strategy
+        handle_response(@strategy.choose(self, options))
+      else
+        nil
+      end
+
+      @choice_in_progress = nil
+      response
+    end
+
+    private
+
+    def handle_response(response)
+      raise "Cannot handle response unless waiting for choice" unless @choice_in_progress
+
+      multiple = @choice_in_progress[:multiple]
+      type = @choice_in_progress[:type]
+      from = @choice_in_progress[:from]
+      max = @choice_in_progress[:max]
+      min = @choice_in_progress[:min]
+      count = @choice_in_progress[:count]
+      restrict_to = @choice_in_progress[:restrict_to]
+      unique = @choice_in_progress[:unique]
+      max_cost = @choice_in_progress[:max_cost]
+      card_type = @choice_in_progress[:card_type]
+
+      if count
+        min = max = count
+      end
+
+      if multiple
+        response = [response] unless response.is_a? Enumerable
+      end
+      
+      # common operation of finding cards in hand by type
+      if type == :card
+        if from == :hand
+          if multiple
+            response = find_cards_in_hand(response)
+          else
+            response = find_card_in_hand(response)
+          end
+        elsif from == :supply
+          raise "Cannot choose multiple cards from supply" if multiple
+          response = peek_from_supply(response)
+        else
+          raise "Cards must be chosen from hand or supply"
+        end
+      end
+
+      if type == :bool
+        if multiple
+          response.each do |r|
+            raise "Response must be an array of true or false values" unless r == true or r == false
+          end
+        else
+          raise "Response must be true or false" unless response == true or response == false
+        end
+      end
+
+      if restrict_to
+        if multiple
+          response.each do |r|
+            raise "Response must be an array of one of: " + restrict_to.to_s unless restrict_to.include?(r)
+          end
+        elsif type == :card
+          raise "Response must be one of: " + restrict_to.to_s unless restrict_to.include?(response.card_class)
+        else
+          raise "Response must be one of: " + restrict_to.to_s unless restrict_to.include?(response)
+        end
+      end
+
+      if type == :card && max_cost
+        raise "Card must cost no more than #{max_cost}, but #{response} costs #{response.cost}" if response.cost > max_cost
+      end
+
+      if type == :card && card_type
+        raise "Card must have type #{card_type}, but #{response} has type #{response.type}" unless response.type.include?(card_type)
+      end
+
+      if multiple && max
+        raise "At most #{max} may be chosen" if response.size > max
+      end
+
+      if multiple && min
+        raise "At least #{min} must be chosen" if response.size < min
+      end
+
+      if multiple && unique
+        raise "Choices must be unique" if response.uniq != response
+      end
+
+      response
+    end
+
+  end
+end
--- lib/player.rb
+++ lib/player.rb
@@ -1,9 +1,11 @@
 require 'util'
+require 'choices'
 
 module Dominion
   
   class Player
     include Util
+    include Choices
     
     attr_accessor :identity, :strategy, :attack_prevented
     
@@ -13,7 +15,7 @@ module Dominion
       :durations_on_first_turn, :durations_on_second_turn,
       :actions_available, :coins_available, :buys_available,
       :vp_tokens, :pirate_ship_tokens,
-      :turn, :card_in_play, :choice_in_progress
+      :turn, :card_in_play
     
     def initialize(game, position, identity, strategy)
       @game = game
@@ -34,7 +36,6 @@ module Dominion
       @pirate_ship_tokens = 0
       @turn = 0
       @card_in_play = nil
-      @choice_in_progress = nil
       @attack_prevented = false
     end
     
@@ -87,7 +88,7 @@ module Dominion
     def end_turn
       check_turn
       raise "Cannot end turn in #{phase} phase" unless action_phase? || treasure_phase? || buy_phase?
-      raise "Cannot end turn while choice in progress" if @choice_in_progress
+      raise "Cannot end turn while choice in progress" if choice_in_progress
 
       move_to_phase :cleanup
       
@@ -340,70 +341,6 @@ module Dominion
       end
     end
     
-    def ask(message, options = {})
-      options[:message] = message
-      options[:type] = :bool
-      options[:multiple] = false
-      choose(options)
-    end
-    
-    def choose_card(message, options = {})
-      options[:message] = message
-      options[:type] = :card
-      options[:multiple] = false
-      choose(options)
-    end
-    
-    def choose_cards(message, options = {})
-      options[:message] = message
-      options[:type] = :card
-      options[:multiple] = true
-      choose(options)
-    end
-
-    def choose_one(messages, symbols, options = {})
-      if symbols.count > 1
-        options[:message] = 'Choose one: ' + messages.join(' or ')
-        options[:messages] = messages
-        options[:type] = :symbol
-        options[:multiple] = false
-        options[:restrict_to] = symbols
-        options[:required] = true
-        choose(options)
-      else
-        # Don't bother asking for zero or one choices
-        symbols.first
-      end
-    end
-
-    def choose_two(messages, symbols, options = {})
-      options[:message] = 'Choose two: ' + messages.join(' or ')
-      options[:messages] = messages
-      options[:type] = :symbol
-      options[:multiple] = true
-      options[:restrict_to] = symbols
-      options[:unique] = true
-      options[:count] = 2
-      choose(options)
-    end
-
-    def choose(options)
-      @choice_in_progress = options
-
-      # use choice if given directly in call to play
-      # otherwise defer to strategy if available
-      response = if @play_choice
-        handle_response(@play_choice)
-      elsif @strategy
-        handle_response(@strategy.choose(self, options))
-      else
-        nil
-      end
-
-      @choice_in_progress = nil
-      response
-    end
-    
     def reveal_from_hand(card_or_class_or_type)
       card = find_card_in_hand(card_or_class_or_type)
       if card
@@ -490,7 +427,7 @@ module Dominion
     
     def check_turn
       raise "It is not #{name}'s turn" unless current_player?
-      raise "Cannot play while choice in progress" if @choice_in_progress
+      raise "Cannot play while choice in progress" if choice_in_progress
     end
 
     def resolve_card_or_class(card_or_class)
@@ -502,88 +439,5 @@ module Dominion
       result
     end
     
-    def handle_response(response)
-      raise "Cannot handle response unless waiting for choice" unless @choice_in_progress
-
-      multiple = @choice_in_progress[:multiple]
-      type = @choice_in_progress[:type]
-      from = @choice_in_progress[:from]
-      max = @choice_in_progress[:max]
-      min = @choice_in_progress[:min]
-      count = @choice_in_progress[:count]
-      restrict_to = @choice_in_progress[:restrict_to]
-      unique = @choice_in_progress[:unique]
-      max_cost = @choice_in_progress[:max_cost]
-      card_type = @choice_in_progress[:card_type]
-
-      if count
-        min = max = count
-      end
-
-      if multiple
-        response = [response] unless response.is_a? Enumerable
-      end
-      
-      # common operation of finding cards in hand by type
-      if type == :card
-        if from == :hand
-          if multiple
-            response = find_cards_in_hand(response)
-          else
-            response = find_card_in_hand(response)
-          end
-        elsif from == :supply
-          raise "Cannot choose multiple cards from supply" if multiple
-          response = peek_from_supply(response)
-        else
-          raise "Cards must be chosen from hand or supply"
-        end
-      end
-
-      if type == :bool
-        if multiple
-          response.each do |r|
-            raise "Response must be an array of true or false values" unless r == true or r == false
-          end
-        else
-          raise "Response must be true or false" unless response == true or response == false
-        end
-      end
-
-      if restrict_to
-        if multiple
-          response.each do |r|
-            raise "Response must be an array of one of: " + restrict_to.to_s unless restrict_to.include?(r)
-          end
-        elsif type == :card
-          raise "Response must be one of: " + restrict_to.to_s unless restrict_to.include?(response.card_class)
-        else
-          raise "Response must be one of: " + restrict_to.to_s unless restrict_to.include?(response)
-        end
-      end
-
-      if type == :card && max_cost
-        raise "Card must cost no more than #{max_cost}, but #{response} costs #{response.cost}" if response.cost > max_cost
-      end
-
-      if type == :card && card_type
-        raise "Card must have type #{card_type}, but #{response} has type #{response.type}" unless response.type.include?(card_type)
-      end
-
-      if multiple && max
-        raise "At most #{max} may be chosen" if response.size > max
-      end
-
-      if multiple && min
-        raise "At least #{min} must be chosen" if response.size < min
-      end
-
-      if multiple && unique
-        raise "Choices must be unique" if response.uniq != response
-      end
-
-      response
-    end
-
   end
 end
